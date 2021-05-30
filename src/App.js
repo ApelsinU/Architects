539b5c55cae2050889f3cdc1ada74f21b5becd31
Vadim <vadimkaptsov@mail.ru>
2021-05-30 15:40:29 +0300

    Deploy source code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import People from './pages/People';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className='app-wrapper'>
+          <Header />
+          <div className='app-wrapper-content'>
+            <Route exact path='/Architects/' component={Home} />
+            <Route exact path='/Architects/people' component={People} />
+          </div>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
